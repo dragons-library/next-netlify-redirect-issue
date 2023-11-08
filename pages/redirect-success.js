384604be45303b89cd8c1db44d0fcf5de3c08f17
dragons-library <97148168+dragons-library@users.noreply.github.com>
2023-11-07 21:23:45 -0800

    add redirect-success route

---
+++ pages/redirect-success.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Redirect Success!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Success!" />
+        <p className="description">
+          You were successfully redirect to <code>/redirect-success</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
